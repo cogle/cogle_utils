7ed50376bce75e01632858beae57e82347660b2c
cogle <thecogle@gmail.com>
2020-05-14 01:42:35 -0700

    Saving work

--- build.py
+++ build.py
@@ -6,6 +6,8 @@ import argparse
 import subprocess
 import multiprocessing
 
+from typing import Dict, List, Optional
+
 #Make sure that each key is reflected in on of the below lists
 COMPILER_FLAG_KEY = "compiler"
 BUILD_FLAG_KEY = "build"
@@ -17,6 +19,8 @@ CLEAN_FLAG_KEY = "clean"
 #TODO CLEAN FLAG MAYBE ALSO STORE LAST BUILD IN JSON FILE TO RELOAD
 #TODO ALLOW BUILD DIR FLAG TO BE PASSED IN
 #TODO PRINT GIT BRANCH at end and start
+#TODO SUPPORT COMPILER SWITCHING VIA ENV SETTING
+#TODO SUPPORT CORES FLAG
 
 CMAKE_BUILD_ARGS_KEYS_SET = {BUILD_FLAG_KEY, TESTS_FLAG_KEY, TSAN_FLAG_KEY, ASAN_FLAG_KEY}
 BUILD_ENV_KEYS_SET = {COMPILER_FLAG_KEY}
@@ -34,6 +38,7 @@ UNIT_TESTS_BUILD = "-DWITH_TESTS=true"
 
 BUILD_CONFIG = ".build.conf.json"
 
+EXIT_CODE_FAIL = -1
 
 class BuildConfig:
     """
@@ -66,7 +71,7 @@ class BuildConfig:
     CMAKE_FLAGS_KEY     = "makeFlags"
     MAKE_FLAGS_KEY      = "makeFlags"
 
-    def __init__(self, json_obj):
+    def __init__(self, json_obj: str):
         self.__dict__ = json_obj
     
     #def update_active(self, build_type, build_dir, cmake_flags, make_flags):
@@ -75,8 +80,33 @@ class BuildConfig:
     #def get_active():
     #    pass
 
+def create_build_dir(args_dict) -> str:
+    build_str = ""
+    if args_dict[BUILD_FLAG_KEY] == CMAKE_DEBUG_BUILD:
+        build_str = "debug"
+    elif args_dict[BUILD_FLAG_KEY] == CMAKE_RELEASE_BUILD:
+        build_str = "release"
+    else:
+        raise ValueError("Key {} does not belong".format(args_dict[BUILD_FLAG_KEY]))
 
-def load_build_config(build_config_path):
+    return "{}_{}_build".format(args_dict[COMPILER_FLAG_KEY], build_str)
+
+def extract_cmake_args(args_dict, previous_build: BuildConfig) -> List[str]:
+    cmake_args = list()
+
+    for k in args_dict:
+        if k in CMAKE_BUILD_ARGS_KEYS_SET:
+            cmake_args.append(args_dict[k])            
+
+    return cmake_args
+
+def extract_env_args(args_dict):
+    #https://stackoverflow.com/questions/7031126/switching-between-gcc-and-clang-llvm-using-cmake
+    env_dict = dict()
+
+    return env_dict
+
+def load_build_config(build_config_path: str) -> Optional[BuildConfig]:
     build_config_file = os.path.join(build_config_path, BUILD_CONFIG)
 
     if os.path.exists(build_config_file):
@@ -85,9 +115,6 @@ def load_build_config(build_config_path):
             return BuildConfig(data)
     else:
         return None
-
-def save_build_config(build_info):
-    pass
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -150,60 +177,24 @@ def parse_args():
 
     return ret
 
-def create_build_dir(args_dict):
-    build_str = ""
-    if args_dict[BUILD_FLAG_KEY] == CMAKE_DEBUG_BUILD:
-        build_str = "debug"
-    elif args_dict[BUILD_FLAG_KEY] == CMAKE_RELEASE_BUILD:
-        build_str = "release"
-    else:
-        raise ValueError("Key {} does not belong".format(args_dict[BUILD_FLAG_KEY]))
-
-    return "{}_{}_build".format(args_dict[COMPILER_FLAG_KEY], build_str)
-
-def extract_cmake_args(args_dict):
-    cmake_args = list()
-
-    for k in args_dict:
-        if k in CMAKE_BUILD_ARGS_KEYS_SET:
-            cmake_args.append(args_dict[k])            
-
-    return cmake_args
-
-def extract_env_args(args_dict):
-    #https://stackoverflow.com/questions/7031126/switching-between-gcc-and-clang-llvm-using-cmake
-    env_dict = dict()
-
-    return env_dict
-
-def perform_build(args_dict):
+def perform_build(args_dict) -> None:
     cur_dir = os.getcwd()
 
     previous_build = load_build_config(cur_dir)
     build_dir = create_build_dir(args_dict)
 
     # Create folder(if needed) and go into that directory
-    #needs_cmake = False
-    #if not os.path.exists(build_dir):
-    #    os.makedirs(build_dir)
-    #    needs_cmake = True
+    if not os.path.exists(build_dir):
+        os.makedirs(build_dir)
 
-    #os.chdir(build_dir)
+    os.chdir(build_dir)
 
-    #if needs_cmake:
-    #    #run_cmake()
-    #    pass
-
-
+    # Run the CMakeCommand with appropriate args
+    cmake_build_commands = extract_cmake_args(args_dict, previous_build)
+    run_cmake(os.path.join(cur_dir, "CMakeLists.txt"), cmake_build_commands)
+    print()
     #run_make()
 
-    #if False:
-    #    pass
-    #else:
-    #    cmake_build_commands = extract_cmake_args(args_dict)
-    #    cmake_command = ["cmake", ".."] + cmake_build_commands
-
-    #    make_command = ["make","-j", "{}".format(max(1, multiprocessing.cpu_count() -2))]
 
     #try:
     #    print("~~~~~~~~~~CMAKE Config~~~~~~~~~~")
@@ -239,6 +230,38 @@ def perform_build(args_dict):
     
     os.chdir(cur_dir)
 
+def run_cmake(cmake_url: str, cmake_build_commands: List[str]) -> None:
+    if not os.path.exists(cmake_url):
+        print("Unable to find CMakeLists.txt at location {}".format(cmake_url))
+        exit(EXIT_CODE_FAIL)
+
+    #TODO: Fix cmd below via 
+    #https://stackoverflow.com/questions/18826789/cmake-output-build-directory
+    #-S for source dir 
+    #-B for build dir
+
+    print("~~~~~~~~~~CMAKE Config~~~~~~~~~~")
+    #cmd = ["cmake", cmake_url] + cmake_build_commands
+    print("~~~~~~~~~~CMAKE Config has completed successfully~~~~~~~~~~")
+
+def run_make():
+    #    make_command = ["make","-j", "{}".format(max(1, multiprocessing.cpu_count() -2))]
+    pass 
+
+def run_subprocess_and_check(cmd: List[str]):
+    try:
+        subprocess.check_call(cmd)
+    except Exception as e:
+        print("\n\n")
+        print(e)
+        print("\n\n")
+        print("<-----------------------------------Exception occurred when running----------------------------------->")
+        print(" ".join(cmd))
+
+        exit(EXIT_CODE_FAIL)
+
+def save_build_config(build_info):
+    pass
 
 if __name__ == "__main__":
     perform_build(parse_args())
