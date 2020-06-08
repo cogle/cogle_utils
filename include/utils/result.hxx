2adad99b4e5ad981d12b4d79c5d462698595e367
cogle <thecogle@gmail.com>
2020-06-07 22:18:32 -0700

    Starting work on the lib

--- include/utils/result.hxx
+++ include/utils/result.hxx
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <type_traits>
+
 namespace cogle {
+
+namespace utils {
+namespace result {
+
+namespace detail {
+enum class ResultTag { OK = 0, ERR = 1 };
+}  // namespace detail
+
+template <typename R>
+struct Ok {
+  using value_type = R;
+
+  [[nodiscard]] constexpr Ok(R val) noexcept(std::is_nothrow_copy_constructible<R>()) : value_(val) {}
+  [[nodiscard]] constexpr Ok(R&& val) noexcept(std::is_nothrow_move_constructible<R>()) : value_(std::move(val)) {}
+
+ private:
+  R value_;
+};
 
 template <typename T, typename E>
 class Result;
@@ -8,4 +28,6 @@ class Result;
 template <typename T, typename E>
 class Result {};
 
+}  // namespace result
+}  // namespace utils
 }  // namespace cogle
