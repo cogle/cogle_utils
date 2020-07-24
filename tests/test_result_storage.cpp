5d5b537b3195a5c78c02908beccfc1d925992ad7
cogle <thecogle@gmail.com>
2020-07-24 09:46:13 -0700

    Continue working on storage test cases

--- tests/test_result_storage.cpp
+++ tests/test_result_storage.cpp
@@ -13,44 +13,48 @@ namespace {
 using namespace cogle::utils::result;
 
 TEST_CASE("ResultStorage Trivial Destruction [result][ResultStorage]") {
-    SECTION("ResultStorage<char, int> Ok<char> copy creation") {
+    SECTION("ResultStorage<char, int> Ok<char> lvalue creation") {
         constexpr auto test_char_a = 'a';
 
         Ok<char> ok{test_char_a};
         detail::ResultStorage<char, int> storage{ok};
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::OK);
         REQUIRE(storage.get_result() == test_char_a);
         REQUIRE(ok.get_result() == test_char_a);
     }
-    SECTION("ResultStorage<char, int> Ok<char> move creation") {
+    SECTION("ResultStorage<char, int> Ok<char> rvalue creation") {
         constexpr auto test_char_a = 'a';
 
         Ok<char> ok{test_char_a};
         detail::ResultStorage<char, int> storage{std::move(ok)};
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::OK);
         REQUIRE(storage.get_result() == test_char_a);
     }
-    SECTION("ResultStorage<char, int> Err<int> copy creation") {
+    SECTION("ResultStorage<char, int> Err<int> lvalue creation") {
         constexpr auto test_int = 1000;
 
         Err<int> err{test_int};
         detail::ResultStorage<char, int> storage{err};
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::ERR);
         REQUIRE(storage.get_error() == test_int);
         REQUIRE(err.get_error() == test_int);
     }
-    SECTION("ResultStorage<char, int> Err<int> move creation") {
+    SECTION("ResultStorage<char, int> Err<int> rvalue creation") {
         constexpr auto test_int = 100;
 
         Err<int> err{test_int};
         detail::ResultStorage<char, int> storage{std::move(err)};
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::ERR);
         REQUIRE(storage.get_error() == test_int);
     }
 }
 
 TEST_CASE("ResultStorage Non-Trivial Destruction [result][ResultStorage]") {
-    SECTION("ResultStorage<NotSoTrivial, int> Ok<NotSoTrivial> copy creation") {
+    SECTION("ResultStorage<NotSoTrivial, int> Ok<NotSoTrivial> lvalue creation") {
         auto int_ptr             = std::make_shared<int>(100);
         const std::string phrase = "This is a test of template deduction";
 
@@ -63,11 +67,12 @@ TEST_CASE("ResultStorage Non-Trivial Destruction [result][ResultStorage]") {
 
         auto& not_trivial_storage = storage.get_result();
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::OK);
         REQUIRE(not_trivial_storage.str == not_trivial.str);
         REQUIRE(not_trivial_storage.not_trivial == not_trivial.not_trivial);
         REQUIRE(*not_trivial_storage.not_trivial == *not_trivial.not_trivial);
     }
-    SECTION("ResultStorage<NotSoTrivial, int> Ok<NotSoTrivial> move creation") {
+    SECTION("ResultStorage<NotSoTrivial, int> Ok<NotSoTrivial> rvalue creation") {
         auto int_ptr             = std::make_shared<int>(100);
         const std::string phrase = "This is a test of template deduction";
 
@@ -80,10 +85,11 @@ TEST_CASE("ResultStorage Non-Trivial Destruction [result][ResultStorage]") {
 
         auto& not_trivial_storage = storage.get_result();
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::OK);
         REQUIRE(not_trivial_storage.str == not_trivial.str);
         REQUIRE(*not_trivial_storage.not_trivial == *int_ptr);
     }
-    SECTION("ResultStorage<int, NotSoTrivial> Err<NotSoTrivial> copy creation") {
+    SECTION("ResultStorage<int, NotSoTrivial> Err<NotSoTrivial> lvalue creation") {
         auto int_ptr             = std::make_shared<int>(100);
         const std::string phrase = "This is a test of template deduction";
 
@@ -96,11 +102,12 @@ TEST_CASE("ResultStorage Non-Trivial Destruction [result][ResultStorage]") {
 
         auto& not_trivial_storage = storage.get_error();
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::ERR);
         REQUIRE(not_trivial_storage.str == not_trivial.str);
         REQUIRE(not_trivial_storage.not_trivial == not_trivial.not_trivial);
         REQUIRE(*not_trivial_storage.not_trivial == *not_trivial.not_trivial);
     }
-    SECTION("ResultStorage<int, NotSoTrivial> Err<NotSoTrivial> move creation") {
+    SECTION("ResultStorage<int, NotSoTrivial> Err<NotSoTrivial> rvalue creation") {
         auto int_ptr             = std::make_shared<int>(100);
         const std::string phrase = "This is a test of template deduction";
 
@@ -113,6 +120,7 @@ TEST_CASE("ResultStorage Non-Trivial Destruction [result][ResultStorage]") {
 
         auto& not_trivial_storage = storage.get_error();
 
+        REQUIRE(storage.get_tag() == detail::ResultTag::ERR);
         REQUIRE(not_trivial_storage.str == not_trivial.str);
         REQUIRE(*not_trivial_storage.not_trivial == *int_ptr);
     }
